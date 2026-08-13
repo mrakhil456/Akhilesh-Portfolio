e47a00199d77ec722e4c9086df96c360c22fdec1
Akhilesh Kumar Sahu <118347309+mrakhil456@users.noreply.github.com>
2026-08-13 15:05:04 +0530

    Add new skills and update existing descriptions

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,14 +1,23 @@
 import { motion } from 'framer-motion';
-import { FaHtml5, FaCss3Alt, FaJs, FaReact, FaPython, FaCode} from 'react-icons/fa';
+import { FaHtml5, FaCss3Alt, FaJs, FaReact, FaPython, FaCode, FaNodeJS, FaBootstrap, FaMobileAlt, FaGithub, FaMicrosoft} from 'react-icons/fa';
+import { SiMongodb, SiVercel } from 'react-icons/si;
 import './styles/Skills.css';
 
 const skills = [
   { icon: <FaHtml5 />, title: 'HTML', description: 'Semantic markup, accessibility, SEO optimization' },
   { icon: <FaCss3Alt />, title: 'CSS', description: 'Flexbox, Grid, animations, responsive design' },
+  { icon: <FaBootstrap />, title: 'Bootstrap', description: 'Responsive Design, Components, Grid System, Utilities' },
+  { icon: <FaMobileAlt />, title: 'Responsive Web Design', description: 'Flexbox, Grid, Media Queries, Mobile-first Design' },
   { icon: <FaJs />, title: 'JavaScript', description: 'ES6+, DOM manipulation, async programming' },
-  { icon: <FaReact />, title: 'React', description: 'Hooks, Context API, Redux, Next.js' },
+  { icon: <FaReact />, title: 'React', description: 'Hooks, Context API, Redux, React Router' },
+  { icon: <FaNodeJS />, title: 'NodeJS ', description: 'REST APIs, Authentication, Server-side Development' },
+  { icon: <SiMongodb />, title: 'MongoDB ', description: 'NoSQL Database, CRUD Operations, Queries, Data Modeling' },
+  { icon: <FaCode />, title: 'C ', description: 'Programming fundamentals, pointers, arrays, functions,' },
   { icon: <FaCode />, title: 'C++', description: 'OOPs, Functions, Basic Memory Management' },
   { icon: <FaPython />, title: 'Python', description: 'OOPs, Functional Programming, Standard Library' },
+  { icon: <SiVercel />, title: 'Vercel', description: 'Deployment, Hosting, CI/CD, Web Applications' },
+  { icon: <FaGithub />, title: 'GitHub', description: 'Version Control, Git, Collaboration, Repository Management' },
+  { icon: <FaMicrosoft />, title: 'MS Office', description: 'Word, Excel, PowerPoint, Document Management' },
 ];
 
 const Skills = () => {
